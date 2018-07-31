66f63057a94fc0740385133c9dbbe3dba19d32c8
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-07-31 17:27:04 -0400

    makeTable added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,9 +22,60 @@ connection.connect(function(err) {
 });
 
 var makeTable = function() {
-    connect.query("SELECT * FROM products", function (err,res){
+    connection.query("SELECT * FROM products", function (err,res){
     for (var i = 0; i ,res.length; i++){
-        console.log(res[i].itemid+"")
+    console.log(res[i].itemID+"||"+
+                res[i].product_name+" || "+
+                res[i].dept_name+" || "+
+                res[i].price+" || "+
+                res[i].stock_qty+"\n");
+                
     }
+    promptCustomer(res);
+
 })
 }
+
+var promptCustomer = (function(res){
+    inquirer.prompt([{
+        type:"input",
+        name: "choice",
+        message:"What would you like to purchase? [Quit with Q]"
+
+    }]).then(function(answer){
+        var correct = false;
+        for(var i=o;i,res.length;i++){
+            if(res[i].product_name==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type:"input",
+                    name:"quant",
+                    message:"How many would you like to buy?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+
+                        }else {
+                            return false;
+
+                        }
+                    }
+                    
+                }).then(function(answer){
+                    if((res[id].stock_qty-answer.quant)>0){
+                        connection.query("UPDATE products SET stock_qty='"+(res[id].stock_qty-answer.quant)
+                        +"'", function(err, res2){
+                            console.log("Product bought!");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("Not a valid selection!");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+    })
+})
---
+++ bamazonSeeds.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon_db;;
+
+CREATE DATABASE bamazon_db;;
+
+USE bamazon_db;;
+
+CREATE TABLE products (
+  itemID integer auto_increment not null,
+  product_name VARCHAR(100) not null,
+  dept_name VARCHAR(100) not null,
+  price DECIMAL (10, 4) not null,
+  stock_qty integer(11) not null,
+  PRIMARY KEY (itemID)
+);
+
+INSERT INTO products (product_name, dept_name, price, stock_qty)
+VALUES ("PS4", "Video Games", 249.99, 176),
+			("NFL Madden 18", "Video Games", 59.99, 50),
+            ("Far Cry 5", "Video Games" ,49.99, 25),
+            ("Grand Theft Auto V", "Video Games", 39.99, 27),
+            ("Call of Duty WWII", "Video Games", 59.99, 10),
+            ("Brake pads", "Car parts", 49.99, 21),
+            ("Castrol oil", "Car parts", 22.65,112),
+            ("Car floor mats", "Car parts", 69.99, 88),
+            ("Dining table", "Home goods", 199.99, 40),
+            ("Chairs", "Home goods", 25.99, 60);
+
+
+SELECT * FROM bamazon_db.products;
